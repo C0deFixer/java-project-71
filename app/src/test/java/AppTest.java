354c656289badc150b29cc3d7f6faeefb4aba6f6
C0deFixer <sharipovdmi@gmail.com>
2023-10-19 10:25:53 +0300

    Checkstyle test fixed

--- app/src/test/java/AppTest.java
+++ app/src/test/java/AppTest.java
@@ -1,30 +1,28 @@
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import hexlet.code.App;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.Map;
-import java.util.stream.Stream;
+
 
 public class AppTest {
-    final String FIRSTFILENAME = "file1.json";
-    final String SECONDFILENAME = "file2.json";
-    final String EXPECTEDRESULTFILENAME = "expected.txt";
+    public static final String FIRST_FILE_NAME = "file1.json";
+    public static final String SECOND_FILENAME = "file2.json";
+    public static final String EXPECTED_RESULTFILENAME = "expected.txt";
+
     @Test
-    public void testCall() throws Exception{
-        Path resourceDirectory = Paths.get("src","test","resources");
+    public void testCall() throws Exception {
+        Path resourceDirectory = Paths.get("src", "test", "resources");
         resourceDirectory.normalize().toString();
         String absolutePath = resourceDirectory.toFile().getAbsolutePath();
 
-        Path firstFilePath = Paths.get(absolutePath,FIRSTFILENAME).toAbsolutePath().normalize();
-        Path secondFilePath = Paths.get(absolutePath,SECONDFILENAME).toAbsolutePath().normalize();
-        Path expectedFilePath = Paths.get(absolutePath,EXPECTEDRESULTFILENAME).toAbsolutePath().normalize();
+        Path firstFilePath = Paths.get(absolutePath, FIRST_FILE_NAME).toAbsolutePath().normalize();
+        Path secondFilePath = Paths.get(absolutePath, SECOND_FILENAME).toAbsolutePath().normalize();
+        Path expectedFilePath = Paths.get(absolutePath, EXPECTED_RESULTFILENAME).toAbsolutePath().normalize();
 
 /*        // Reading content
         String firstFileContent = Files.readString(firstFilePath);
@@ -36,11 +34,11 @@ public class AppTest {
         Map<String, String> map2 = objectMapper.readValue(secondFileContent, new TypeReference<Map<String, String>>() {
         });*/
 
-        String expected = Files.readString(expectedFilePath);;
+        String expected = Files.readString(expectedFilePath);
 
-        String actual = App.generate(firstFilePath.toString(),secondFilePath.toString());
+        String actual = App.generate(firstFilePath.toString(), secondFilePath.toString());
 
-        Assertions.assertLinesMatch(Arrays.stream(actual.split("\n")),Arrays.stream(expected.split("\n")));
+        Assertions.assertLinesMatch(Arrays.stream(actual.split("\n")), Arrays.stream(expected.split("\n")));
 
     }
 }
