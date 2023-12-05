793ab66a798d10da3508b8f5039d25dd31fb0d49
Dmitry Sharypov <Sharypov_D@mail.ru>
2023-12-05 23:00:43 +0300

    Visibility Modifier fixed

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -11,16 +11,16 @@ import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
-public class App implements Callable<String> {
+public final class App implements Callable<String> {
     @Parameters(paramLabel = "filepath1", description = "path to first file")
-    String firstFile;
+    private String firstFile;
     @Parameters(paramLabel = "filepath2", description = "path to second file")
-    String secondFile;
+    private String secondFile;
     @Option(names = {"-f", "--format"},
             paramLabel = "format",
             defaultValue = "stylish",
             description = "output format [default: stylish]")
-    String format;
+    private String format;
 
     @Override
     public String call() throws Exception { // your business logic goes here...
