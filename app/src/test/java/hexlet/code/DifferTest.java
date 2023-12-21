8ceb330ffc1e3beeb1b9b76d145a751b13ff8f98
C0deFixer <sharipovdmi@gmail.com>
2023-12-21 10:06:16 +0300

    fix linter issues

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,11 +1,6 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.assertj.core.api.MapAssert;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.skyscreamer.jsonassert.JSONAssert;
@@ -14,11 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.Map;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.in;
 
 class DifferTest {
 
@@ -28,24 +19,30 @@ class DifferTest {
 
     @ParameterizedTest
     @CsvSource({
-            ", file1.json, file2.json, expected_json.txt",
-            "plain, file1.yaml, file2.yaml, expected_yaml_plain.txt",
-            "stylish, file1.yaml, file2.yaml, expected_yaml_stylish.txt",
-            "json, file1.yaml, file2.yaml, expected_yaml_json.json"
+        ", file1.json, file2.json, expected_json.txt",
+        "plain, file1.yaml, file2.yaml, expected_yaml_plain.txt",
+        "stylish, file1.yaml, file2.yaml, expected_yaml_stylish.txt",
+        "json, file1.yaml, file2.yaml, expected_yaml_json.json"
     })
-    public void testGenerate(String format, String firstFileName, String secondFileName, String expectedFileName) throws Exception {
+    public void testGenerate(String format,
+                             String firstFileName,
+                             String secondFileName,
+                             String expectedFileName) throws Exception {
         String expected = readFileContent(expectedFileName);
         String actual;
         if (format == null || format instanceof String && format.isEmpty()) {
-            actual = Differ.generate(getFilePath(firstFileName).toString(), getFilePath(secondFileName).toString());
+            actual = Differ.generate(getFilePath(firstFileName).toString(),
+                    getFilePath(secondFileName).toString());
         } else {
-            actual = Differ.generate(getFilePath(firstFileName).toString(), getFilePath(secondFileName).toString(), format);
+            actual = Differ.generate(getFilePath(firstFileName).toString(),
+                    getFilePath(secondFileName).toString(), format);
         }
 
         if (format != null && format.equals(JSON_FORMAT)) {
             JSONAssert.assertEquals(expected, actual, false);
         } else {
-            Assertions.assertLinesMatch(Arrays.stream(actual.split("\n")), Arrays.stream(expected.split("\n")));
+            Assertions.assertLinesMatch(Arrays.stream(actual.split("\n")),
+                    Arrays.stream(expected.split("\n")));
         }
     }
 
